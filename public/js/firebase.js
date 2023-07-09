1882688a218ee6fd824303a856d25303db630454
Shantanu Ranjan <32164926+shantanuxix@users.noreply.github.com>
2023-07-09 12:07:03 +0530

    Update firebase.js

--- public/js/firebase.js
+++ public/js/firebase.js
@@ -1,11 +1,11 @@
 let firebaseConfig = {
-        apiKey: "AIzaSyD0prVU3wJ-I3VHTU2hh3m_EK1Q2lPBOOA",
-        authDomain: "blog-f5718.firebaseapp.com",
-        projectId: "blog-f5718",
-        storageBucket: "blog-f5718.appspot.com",
-        messagingSenderId: "481726901857",
-        appId: "1:481726901857:web:4807a66968d3300b188420",
-        measurementId: "G-RQB51PP43M"
+        apiKey: "Enter Your Information",
+        authDomain: "Enter Your Information",
+        projectId: "Enter Your Information",
+        storageBucket: "Enter Your Information",
+        messagingSenderId: "Enter Your Information",
+        appId: "Enter Your Information",
+        measurementId: "Enter Your Information"
 };
 
 firebase.initializeApp(firebaseConfig);
